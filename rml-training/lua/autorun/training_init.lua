309421bf0abdaf1cb09e25e5a98401e276927b02
Charles Mills <charlesrjmills@gmail.com>
2024-08-22 02:19:02 +0100

    Refactor code for better organization and readability

--- rml-training/lua/autorun/training_init.lua
+++
@@ -1,9 +0,0 @@
-if SERVER then
-    AddCSLuaFile("skidnetworks_training/cl_marker.lua")
-    AddCSLuaFile("skidnetworks_training/cl_training.lua")
-    AddCSLuaFile("skidnetworks_training/config.lua")
-    include("skidnetworks_training/sv_training.lua")
-else
-    include("skidnetworks_training/cl_marker.lua")
-    include("skidnetworks_training/cl_training.lua")
-end
